3d3dd76f3a34b90b99f906723c3648fc5ca7cfa4
yeahzzz <yanzhang.scut@gmail.com>
2014-07-17 19:19:12 +0800

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,49 +1,32 @@
-#include "zmq.hpp"
+#include <vector>
+#include <string>
 #include <iostream>
-#include <sstream>
-#include "message.pb.h"
+#include <stdio.h>
+#include "subapi.h"
 
-static char *
-s_recv (void *socket) {
-    char buffer [256];
-    int size = zmq_recv (socket, buffer, 255, 0);
-    if (size == -1)
-        return NULL;
-    if (size > 255)
-        size = 255;
-    buffer [size] = 0;
-    return strdup (buffer);
-}
-
-int main (int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-    //  Socket to talk to server
-    printf ("Collecting updates from weather server...\n");
-    void *context = zmq_ctx_new ();
-    void *subscriber = zmq_socket (context, ZMQ_SUB);
-    int rc = zmq_connect (subscriber, "tcp://localhost:8231");
-    assert (rc == 0);
-
-    //  Subscribe to zipcode, default is NYC, 10001
-    const char *filter = "";
-    rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE,
-                         filter, strlen (filter));
-    assert (rc == 0);
-
-    while(1) {
-    	stocksim::Stock stock_msg;
-        char *string = s_recv (subscriber);
-
-        printf("string buf size: %d %d\n", sizeof(string), strlen(string));
-        bool ret = stock_msg.ParseFromArray(string, strlen(string));
-        std::cout << "code: " << stock_msg.code() << std::endl;
-        std::cout << "name: " << stock_msg.name() << std::endl;
-        std::cout << "in_price: " << stock_msg.in_price() << std::endl;
-        std::cout << "out_price: " << stock_msg.out_price() << std::endl;
-        std::cout << "trade: " << stock_msg.trade() << std::endl;
-        free (string);
+    std::vector<std::string> sub_list;
+    std::string conn = "tcp://localhost:8231";
+    char code[5] = "8000";
+    char c;
+    int i;
+    for (i = 0; i < 10; i++) {
+    	printf("[%d] name: stock_%d\n", i, i);
+	}
+	std::cout << "choose your stock, split by space (end with Enter): " << std::endl;
+    for(;;){
+    	c = getchar();
+    	if (c == 10) {
+    		std::cout << "quit" << std::endl;
+    		break;
+    	}
+    	code[3] = c;
+    	sub_list.push_back(code);
     }
-    zmq_close (subscriber);
-    zmq_ctx_destroy (context);
+    std::cout << "thanks for your subscription!" << std::endl;
+    //sub_list.push_back("8001");
+    //sub_list.push_back("8008");
+    sub_stock(conn, sub_list);
     return 0;
 }
